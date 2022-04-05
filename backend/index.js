3516c20be09997d0e35a5e657158824f2695aa0a
“Tharanka <rtharanka93@gmail.com>
2022-04-05 11:21:23 -0400

    added more dep

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@
 // (if you call it server then have to change it on package)
 
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -9,11 +11,21 @@ const PORT = 8000;
 
 const app = express();
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected!");
+  })
+  .catch((err) => console.log(err));
+
 app.use(morgan("tiny"));
 app.use(helmet());
 app.use(cors());
-
-app.get("/grwgrw", eew);
 
 // app.router...can use this to organize into other folders
 
